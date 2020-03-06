e9dc0d6c889730a9a5490a5bd3eb588ba1b53a3d
qiaw99 <46455899+qiaw99@users.noreply.github.com>
2020-03-06 15:44:02 +0100

    Update db.c

--- src/db.c
+++ src/db.c
@@ -3,6 +3,11 @@
 #include <string.h>
 #include<stdbool.h>
 #include<stdint.h>
+#include<sys/types.h>
+#include<unistd.h>
+#include<sys/stat.h>
+#include<fcntl.h>
+#include <errno.h>
 
 #define COLUMN_USERNAME_SIZE 32
 #define COLUMN_EMAIL_SIZE 255
@@ -10,9 +15,9 @@
 #define size_of_attribute(Struct, Attribute) sizeof(((Struct*)0) -> Attribute)
 
 typedef struct {
-		uint32_t id;
-		char username[COLUMN_USERNAME_SIZE + 1];
-		char email[COLUMN_EMAIL_SIZE + 1];
+	uint32_t id;
+	char username[COLUMN_USERNAME_SIZE + 1];
+	char email[COLUMN_EMAIL_SIZE + 1];
 }Row;
 
 uint32_t ID_SIZE;
@@ -54,12 +59,18 @@ typedef enum {
 	STATEMENT_SELECT
 }StatementType;
 
+typedef struct{
+	int file_description;
+	uint32_t file_length;
+	void* pages[TABLE_MAX_PAGES];
+}Pager;
+
 /*Table structure contains pointers to pages of raws and keeps
   tracks of how many row there are.	
 */
 typedef struct {
 	uint32_t num_rows;
-	void* pages[TABLE_MAX_PAGES];
+	Pager* pager;
 }Table;
 
 typedef struct{
@@ -73,15 +84,52 @@ typedef struct{
 	ssize_t input_length;
 }InputBuffer;
 
+/*
+Handle cache miss. Assume that pages are saved one after the other in the
+database file: Page 0 at offset 0,Page 1 at 4096 etc. If the requested page
+lies outside the bounds of file, we just allocate some memory and return it.
+The page will be added to the file when we flush the cache to disk later. 
+*/
+void* get_page(Pager* pager, uint32_t page_num){
+	if(page_num > TABLE_MAX_PAGES){
+		printf("Tried to fetch page number out of bounds. %d > %d"
+			, page_num, TABLE_MAX_PAGES);
+		exit(EXIT_FAILURE);
+	}
+
+	if(pager -> pages[page_num] == NULL){
+		// Cache miss. Allocate memory and load from file
+		void* page = malloc(PAGE_SIZE);
+		uint32_t num_pages = pager -> file_length / PAGE_SIZE;
+
+		// We might save a partial page at the end of the file
+		if(pager -> file_length % PAGE_SIZE){
+			num_pages += 1;
+		}
+
+		if(page_num <= num_pages){
+			lseek(pager -> file_description, page_num * PAGE_SIZE, SEEK_SET);
+			ssize_t bytes_read = read(pager -> file_description, page, PAGE_SIZE);
+
+			if(bytes_read == -1){
+				printf("Error reading file %d \n", errno);
+				exit(EXIT_FAILURE);
+			}
+		}
+		pager -> pages[page_num] = page;
+	}
+	return pager -> pages[page_num];
+}
+
+
 void* row_slot(Table* table, uint32_t row_num){
 	uint32_t page_num = row_num / ROWS_PER_PAGE;
-	void* page = table -> pages[page_num];
-	if(page == NULL){
-		// Allocate the memory only if when we try to access page
-		page = table -> pages[page_num] = malloc(PAGE_SIZE);
-	}
-	return  page +  row_num % ROWS_PER_PAGE * ROW_SIZE;
+	void* page = get_page(table -> pager, page_num);
+	uint32_t row_offset = row_num % ROWS_PER_PAGE;
+	uint32_t byte_offset = row_offset * ROW_SIZE;
+	return  page +  byte_offset;
 }
+
 
 // convert to or from the compact representation
 void serialize_row(Row* source, void* destination){
@@ -178,22 +226,64 @@ void print_row(Row* row){
 	printf("(%d %s %s)\n", row -> id, row -> username, row -> email);
 }
 
-Table* new_table(){
-	Table* table = malloc(sizeof(Table));
-	table -> num_rows = 0;
-	for(uint32_t i = 0; i < TABLE_MAX_PAGES; i++){
-		table -> pages[i] = NULL;
+/* 
+Open the database file and keep track of its size,
+and also intialize the page cache to all NULL
+*/
+Pager* pager_open(const char* filename){
+	/*
+	Read/write mode, create file if it does not exist, 
+	user write permission, user read permission
+	*/
+	int fd = open(filename, O_RDWR | O_CREAT, S_IWUSR | S_IRUSR);
+
+	if(fd == -1){
+		printf("Unable to open the file!\n");
+		exit(EXIT_FAILURE);
 	}
+
+	off_t file_length = lseek(fd, 0, SEEK_END);
+	Pager* pager = malloc(sizeof(Pager));
+	pager -> file_description = fd;
+	pager -> file_length = file_length;
+
+	for(uint32_t i = 0; i < TABLE_MAX_PAGES; i ++){
+		pager -> pages[i] = NULL;
+	}
+
+	return pager;
+}
+
+/*
+Open a connection to the database.
+1. Opening the database file
+2. Initializing a page data structure
+3. Initializing a table data structure
+*/
+Table* db_open(const char* filename){
+	Pager* pager = pager_open(filename);
+	uint32_t num_rows = pager -> file_length / ROW_SIZE;
+
+	Table* table = malloc(sizeof(Table));
+	table -> num_rows = num_rows;
+	table -> pager = pager;
 	return table;
 }
 
+
+
+void db_close(Table* table){
+	Pager* pager = table -> pager;
+	// TODO
+}
+/*
 void free_table(Table* table){
-	for(int i = 0; table -> pages[i]; i ++){
-		free(table -> pages[i]);
+	for(int i = 0; table -> pager[i]; i ++){
+		free(table -> pager[i]);
 	}
 	free(table);
 }
-
+*/
 InputBuffer* new_input_buffer(){
 	InputBuffer* input_buffer = (InputBuffer*)malloc(sizeof(InputBuffer));
 	input_buffer -> buffer = NULL;
@@ -243,7 +333,13 @@ int main(int argc, char* argv[]){
 	TABLE_MAX_ROWS = ROWS_PER_PAGE * TABLE_MAX_PAGES;
 
 
-	Table* table = new_table();
+	if(argc < 2){
+		printf("Must supply a database name!.\n");
+		exit(EXIT_FAILURE);
+	}
+
+	char* filename = argv[1];
+	Table* table = db_open(filename);
 	InputBuffer* input_buffer = new_input_buffer();
 	while(true){
 		print_prompt();
